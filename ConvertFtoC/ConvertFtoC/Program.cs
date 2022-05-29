9044d2fd0cedcf1b88d1f54edb99e9ce6350ee73
GDJEvans <gdjevans@gmail.com>
2022-05-29 23:03:11 +0100

    Push Start of Data Types section

---
+++ ConvertFtoC/ConvertFtoC/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConvertFtoC
+{
+    class Program
+    {
+        public static void Main()
+        {
+            char char1 = 'a';
+            char char2 = '\n';
+            string s1 = "abc";
+        }
+
+
+        public convertToCelcius(fahrenheit)
+        {
+            return (fahrenheit - 32) * (5 / 9);
+        }
+    }
+}
