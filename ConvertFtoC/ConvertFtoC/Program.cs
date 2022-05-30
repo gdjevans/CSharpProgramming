66d6d2dcc3d163d6055a8278aec7298a6a27fd06
GDJEvans <gdjevans@gmail.com>
2022-05-30 10:38:47 +0100

    Finish Data Types section and start and finish DateTime section.

--- ConvertFtoC/ConvertFtoC/Program.cs
+++ ConvertFtoC/ConvertFtoC/Program.cs
@@ -6,15 +6,46 @@ namespace ConvertFtoC
     {
         public static void Main()
         {
-            char char1 = 'a';
-            char char2 = '\n';
-            string s1 = "abc";
+            //char char1 = 'a';
+            //char char2 = '\n';
+            //string s1 = "abc";
+            int i = 200;
+            System.Diagnostics.Debug.WriteLine(i);
+            ChangeValue(i);
+            System.Diagnostics.Debug.WriteLine(i);
         }
 
-
-        public convertToCelcius(fahrenheit)
+        int fahrenheit = 0;
+        public static class convertToCelcius(fahrenheit)
         {
             return (fahrenheit - 32) * (5 / 9);
         }
+
+        public static class Exercise
+        {
+            public static void StringChallenge()
+            {
+                string s = "Hello World";
+                Console.WriteLine(s.Substring(0, 5));
+            }
+        }
+
+        public static class Exercise2
+        {
+            public static void BoxingChallenge()
+            {
+                int example = 1;
+                object o = example;
+                int example2 = (int)o;
+            }
+        }
+
+        static void ChangeValue(int num)
+        {
+            num = 500;
+            System.Diagnostics.Debug.WriteLine(num);
+        }
+
     }
+
 }
