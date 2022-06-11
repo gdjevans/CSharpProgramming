5096b20cdeeaa6cc5d3a3eb3c9797f72e1f43c0c
GDJEvans <gdjevans@gmail.com>
2022-06-11 10:57:17 +0100

    Advanced C# and Regex

---
+++ Advanced/Advanced/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Advanced
+{
+
+    class Program { 
+        static int fib(int n)
+        {
+            if (n == 0 || n == 1)
+            {
+                return n;
+            } 
+
+            return fib(n - 1) + fib(n - 2);
+        }
+
+        public static void Main(String[] args)
+        {
+            System.Diagnostics.Debug.WriteLine(fib(11));
+
+        }
+
+    }
+
+}
